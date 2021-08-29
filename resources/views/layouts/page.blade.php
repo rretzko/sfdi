b407f24ade1920915d3d435a04031de5b9a88721
MFRHoldings <rick@mfrholdings.com>
2021-08-29 12:36:29 -0400

    Add main.css to layouts.page

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -26,6 +26,7 @@
 	<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
 	<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/main.css" rel="stylesheet" type="text/css" />
 
 </head>
 
