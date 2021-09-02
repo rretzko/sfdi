82a633464e1546fd22dbbe97354d9d568c777ef4
MFRHoldings <rick@mfrholdings.com>
2021-09-02 18:15:06 -0400

    Hardcode general links on fullensemble.blade.php and sheetmusic.blade.php

--- resources/views/partials/fullensemble.blade.php
+++ resources/views/partials/fullensemble.blade.php
@@ -1,7 +1,7 @@
 <li>
     <label>Full Ensemble</label>
     <audio controls>
-        <source src="{{ asset('pitchfiles/nja-sc/quintet_Swan/fullEnsemble/Swan.ALL_.mp3') }}" type="audio/mpeg">
-        <source src="{{ asset('pitchfiles/nja-sc/quintet_Swan/fullEnsemble/Swan.ALL_.mp3') }}" type="audio/ogg">
+        <source src="https://studentfolder.info/assets/pitchfiles/nja-sc/quintet_Swan/fullEnsemble/Swan.ALL_.mp3" type="audio/mpeg">
+        <source src="https://studentfolder.info/assets/pitchfiles/nja-sc/quintet_Swan/fullEnsemble/Swan.ALL_.mp3" type="audio/ogg">
     </audio>
 </li>
