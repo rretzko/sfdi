38afc4ea0fddad537f9efe49d2fa1c5cc29b4fd8
MFRHoldings <rick@mfrholdings.com>
2021-09-02 18:21:12 -0400

    Add target="_BLANK" to general links on fullensemble.blade.php and sheetmusic.blade.php

--- resources/views/partials/fullensemble.blade.php
+++ resources/views/partials/fullensemble.blade.php
@@ -1,7 +1,7 @@
 <li>
     <label>Full Ensemble</label>
     <audio controls>
-        <source src="https://studentfolder.info/assets/pitchfiles/nja-sc/quintet_Swan/fullEnsemble/Swan.ALL_.mp3" type="audio/mpeg">
-        <source src="https://studentfolder.info/assets/pitchfiles/nja-sc/quintet_Swan/fullEnsemble/Swan.ALL_.mp3" type="audio/ogg">
+        <source src="https://studentfolder.info/assets/pitchfiles/nja-sc/quintet_Swan/fullEnsemble/Swan.ALL_.mp3" target="_BLANK" type="audio/mpeg">
+        <source src="https://studentfolder.info/assets/pitchfiles/nja-sc/quintet_Swan/fullEnsemble/Swan.ALL_.mp3" target="_BLANK" type="audio/ogg">
     </audio>
 </li>
--- resources/views/partials/sheetmusic.blade.php
+++ resources/views/partials/sheetmusic.blade.php
@@ -1,5 +1,5 @@
 <li>
-    <a href="https://studentfolder.info/assets/pitchfiles/nja-sc/quintet_Swan/sheetMusic/The-Silver-Swan-Sheet-Music.pdf">
+    <a href="https://studentfolder.info/assets/pitchfiles/nja-sc/quintet_Swan/sheetMusic/The-Silver-Swan-Sheet-Music.pdf" target="_BLANK" >
         Sheet Music
     </a>
 </li>
