7bc2b8b24f1aa945c140588f4c77f8195dcdf068
MFRHoldings <rick@mfrholdings.com>
2021-09-06 16:40:33 -0400

    Reset script path for teachersFromSchool.js #2

--- resources/views/pages/addeditschools.blade.php
+++ resources/views/pages/addeditschools.blade.php
@@ -80,7 +80,7 @@ Add School
 <link href="{{ asset('/plugins/select2/select2.min.css') }}" rel="stylesheet" type="text/css" />
 <script src="{{ asset('/plugins/select2/select2.min.js') }}"></script>
 <!-- <script src="{{asset('/js/teachersFromSchool.js')}}"></script> -->
-<script src="https://studentfolder.info/js/teachersFromSchool.js" ></script>
+<script src="https://studentfolder.info/assets/js/teachersFromSchool.js" ></script>
 <!-- <script src="http://localhost/dev/sfdi/public/assets/js/teachersFromSchool.js"></script> -->
 <script type="text/javascript">
     $(document).ready(function() {
