4df68fa42438085c832de2ffa15279a85d667839
MFRHoldings <rick@mfrholdings.com>
2021-09-07 08:16:19 -0400

    Add console.log tracing in teachersFromSchool.js

--- public/assets/js/teachersFromSchool.js
+++ public/assets/js/teachersFromSchool.js
@@ -7,7 +7,7 @@ var PATH = 'http://localhost/dev/sfdi/public/'; //https://studentfolder.info/';
 
 
 function teachersFromSchool()
-{
+{console.log('@ teachersFromSchool');
     //advisories
     var advisory = 'If not listed above, please ask your teacher to sign up at <a href="https:\\TheDirectorsRoom.com" title="Sign up at TheDirectorsRoom.com" target="_blank" tabindex="-1">TheDirectorsRoom.com</a>.';
     var advisory_none = 'Please ask your teacher to sign up at <a href="https:\\TheDirectorsRoom.com" title="Sign up at TheDirectorsRoom.com" target="_blank" tabindex="-1">TheDirectorsRoom.com</a>.';
@@ -18,20 +18,21 @@ function teachersFromSchool()
     }else{
         var $path = 'https://studentfolder.info/TeachersFromSchool';
     }
+
+console.log('path: '+$path);
     //clear any existing messages
     $('.instructionTeacher').css('display', 'none');
-alert($path);
+//alert($path);
     $.ajax({
        method: 'POST',
-       //url: PATH+'TeachersFromSchool',
-        url: $path,
+       url: $path,
        data: {
            'school_id' : $('#id').val(),
             '_token' : $("input[name='_token']").val()
         },
 
        success: function(response){
-//console.log('26. '+PATH+'TeachersFromSchool');
+console.log('35 success. ');
            var obj = JSON.parse(response); //create json object
 
            $('.teacherlist').html(obj);
