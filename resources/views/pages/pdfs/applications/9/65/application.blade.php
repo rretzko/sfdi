5bca0da767dc750f6947c0e97d5e89b61d3ab4eb
MFRHoldings <rick@mfrholdings.com>
2021-09-08 07:33:35 -0400

    Fix njmea logo location #2

--- resources/views/pages/pdfs/applications/9/65/application.blade.php
+++ resources/views/pages/pdfs/applications/9/65/application.blade.php
@@ -39,6 +39,7 @@
         <td style="width: 10%;">
             <!-- <img src="https:\\studentfolder.info\assets\images\njmea_logo_state.jpg" alt="NJMEA Logo"/> -->
             <img src="https:\\studentfolder.info\njmea_logo_state.jpg" alt="NJMEA Logo"/>
+            <img src="assets/images/njmea_logo_state.jpg" alt="NJMEA Logo" />
         </td>
         <td style="width: 72%;">
             <div style="font-weight: bold; text-align: center">{{ $registrant->student->person->user->schools->first()->name }}</div>
