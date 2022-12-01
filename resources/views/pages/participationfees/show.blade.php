97e8261a90d01da5b45ba479e54428ec75efa0f3
MFRHoldings <rick@mfrholdings.com>
2022-12-01 16:23:33 -0500

    Added styling for PayPal Participation Fee page (4).

--- resources/views/pages/participationfees/show.blade.php
+++ resources/views/pages/participationfees/show.blade.php
@@ -7,31 +7,31 @@
 @section('content')
 
     @auth
-        <div style="margin-top: 1rem;">
-            <div style="background-color: white; padding: 1rem; padding-bottom: 3rem; margin-top: 1rem;" >
-                <h2>
-                    Congratulation on your acceptance into the {{ $eventversion->name }}!
-                </h2>
 
-                <h3 style="text-align: center;">
-                    @if(auth()->user()->currentRegistrant($eventversion)->participationFeePaid())
-                        Thank you for paying your ${{ $eventversion->eventversionconfig->participation_fee_amount }}
-                        participation fee!
-                    @else
-                        Please click the PayPal button below to pay your
-                        ${{ $eventversion->eventversionconfig->participation_fee_amount }}
-                        participation fee for this year's event.
-                        <div style="margin: auto;">
-                            <x-paypals.25.73.paypal_button
-                                amountduenet="{{ $eventversion->eventversionconfig->participation_fee_amount }}"
-                                :eventversion="$eventversion"
-                                :registrant="auth()->user()->currentRegistrant($eventversion)"
-                                :school="auth()->user()->currentSchool($eventversion)"
-                            />
-                        </div>
-                    @endif
-                </h3>
-            </div>
+        <div style="margin-bottom: 1rem;">{{-- SPACER --}}</div>
+        <div style="background-color: white; padding: 1rem; padding-bottom: 3rem; margin-top: 1rem;" >
+            <h2>
+                Congratulation on your acceptance into the {{ $eventversion->name }}!
+            </h2>
+
+            <h3 style="text-align: center;">
+                @if(auth()->user()->currentRegistrant($eventversion)->participationFeePaid())
+                    Thank you for paying your ${{ $eventversion->eventversionconfig->participation_fee_amount }}
+                    participation fee!
+                @else
+                    Please click the PayPal button below to pay your
+                    ${{ $eventversion->eventversionconfig->participation_fee_amount }}
+                    participation fee for this year's event.
+                    <div style="margin: auto;">
+                        <x-paypals.25.73.paypal_button
+                            amountduenet="{{ $eventversion->eventversionconfig->participation_fee_amount }}"
+                            :eventversion="$eventversion"
+                            :registrant="auth()->user()->currentRegistrant($eventversion)"
+                            :school="auth()->user()->currentSchool($eventversion)"
+                        />
+                    </div>
+                @endif
+            </h3>
         </div>
 
     @endauth
