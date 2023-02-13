81eb826e94964a4fb9ee530a80cc20f238fc3b80
MFRHoldings <rick@mfrholdings.com>
2023-02-13 10:29:08 -0500

    Corrected application text for NJ All-State in application.blade.php.

--- resources/views/pages/pdfs/applications/9/75/application.blade.php
+++ resources/views/pages/pdfs/applications/9/75/application.blade.php
@@ -100,13 +100,13 @@
                 <li>
                     I, <b>{{ $registrant->student->person->fullName }}</b>, agree to accept the decision of the
                     judges as binding. If selected, I will accept membership in the {{ $eventversion->name }} for which I have
-                    auditioned. I also agree to pay the ${{ number_format($eventversion->eventversionconfig->registrationfee,2) }}
+                    auditioned. I also agree to pay the $35 (subject to change)
                     participation fee. I understand that membership in this organization may be terminated by the endorsers
                     of my application if I fail to comply with the rules set forth or if I fail to learn my music.
                 </li>
                 <li>
                     I understand that NJ All-State Mixed Chorus members are expected to attend all rehearsals
-                    from June through November. NJ All-State Treble Chorus rehearsals continue until February. One
+                    from October through November. NJ All-State Treble Chorus rehearsals are held in February. One
                     absence will result in testing at the following rehearsal. An absence is defined as missing any
                     scheduled rehearsal or any part thereof. I further understand that all activities must be attended
                     in their entirety. I understand that it is not possible for me to be a member of the NJ All-State
@@ -241,7 +241,7 @@
             read the endorsement, which {{ $registrant->student->person->first }} has signed, and I give permission
             for {{ $registrant->student->person->pronoun->object }} to audition to become a member of the
             {{ $eventversion->name }}. I promise to assist {{ $registrant->student->person->first }} in
-            fulfilling All-State obligations and in meeting any expenses necessary for rehearsals and concerts. I
+            fulfilling {{ $registrant->student->person->pronoun->possessive }} All-State obligations and in meeting any expenses necessary for rehearsals and concerts. I
             understand it is the policy of NJMEA that if an All-State student is incapacitated in any way that
             requires additional assistance, it will be the responsibility of the All-State student's parent/guardian/school
             to provide the necessary help at all rehearsals, meals, concerts, etc. The provided chaperone will be
@@ -258,7 +258,7 @@
     </tr>
     <tr class="sectionSignaturesSubscript">
         <td colspan="2" style="font-size: .8rem;">
-            PARENT/LEGAL GUARDIAN CELL: {{ $registrant->student->guardians->count() ? $registrant->student->guardians->first()->phoneMobile : 'No number found' }}
+            PARENT/LEGAL GUARDIAN CELL: {{ ($registrant->student->guardians->count() && strlen($registrant->student->guardians->first()->phoneMobile)) ? $registrant->student->guardians->first()->phoneMobile : 'No number found' }}
         </td>
     </tr>
 </table>
@@ -282,10 +282,10 @@
             enrolled in grade {{ $registrant->student->gradeClassof }} at {{ $registrant->student->person->user->schools->first()->name }}.
             We understand that <b>{{ $registrant->student->currentTeachername }}</b>, who is sponsoring <b>{{ $registrant->student->person->fullName }}</b>,
             is a current (paid) member of the National Association for Music Educators (NAfME), and is required to
-            participate as a JUDGE FOR ONLINE AUDITIONS, as described in the Directors's Packet, from April 28-30, 2022.
+            participate as a JUDGE FOR ONLINE AUDITIONS, as described in the Director's Packet, from April 27-29, 2022.
             <br />
             We will review this application to ensure that all parts are complete and accurate. This application
-            will be mailed to the Registration Manager postmarked by the application deadline of <b>April 1st, 2022.</b>
+            will be mailed to the Registration Manager postmarked by the application deadline of <b>March 31st, 2023.</b>
             LATE APPLICATIONS WILL NOT BE ACCEPTED. If <b>{{ $registrant->student->person->fullName }}</b> is accepted,
             we will ensure that <b>{{ $registrant->student->person->first }}</b> is prepared and adheres to
             the rules and regulations set forth by the NJMEA.
